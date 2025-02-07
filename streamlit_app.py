2cb5e5daa7e1df2421ae2f25ccdd703a8dc1aec7
chigkim <chigkim@users.noreply.github.com>
2025-02-07 11:29:01 -0500

    Sidebar UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,8 @@ def play(audio_data):
     st.markdown(md, unsafe_allow_html=True)
 
 
-def process(audio):
+def transcribe(audio):
+    r = sr.Recognizer()
     with sr.AudioFile(audio) as data:
         source = r.record(data)
         text = r.recognize_google(source, language="en")
@@ -34,23 +35,55 @@ def process(audio):
     return text
 
 
+def handle_audio():
+    with st.sidebar:
+        if audio := mic_recorder(
+            start_prompt="🎙 Record",
+            stop_prompt="📤 Stop",
+            just_once=True,
+            use_container_width=True,
+            format="wav",
+            key="recorder",
+        ):
+            audio_bio = BytesIO(audio["bytes"])
+            audio_bio.name = "audio.wav"
+            return transcribe(audio_bio)
+
+
+def show_messages():
+    for message in st.session_state.messages:
+        with st.chat_message(name="Bot"):
+            st.markdown(message)
+
+
+def toggle_text_chat():
+    st.session_state.text_chat_enabled = not st.session_state.text_chat_enabled
+
+
 st.set_page_config(
     page_title="Echo",
     page_icon=":material/speaker:",
+    layout="wide",
+    initial_sidebar_state="expanded",
 )
 st.title("Echo")
 load_css("style.css")
-r = sr.Recognizer()
-if audio := mic_recorder(
-    start_prompt="🎙 Record",
-    stop_prompt="📤 Stop",
-    just_once=False,
-    use_container_width=True,
-    format="wav",
-    key="recorder",
-):
-    audio_bio = BytesIO(audio["bytes"])
-    audio_bio.name = "audio.wav"
-    text = process(audio_bio)
-    if st.button(text):
-        st.markdown(text)
+if "text_chat_enabled" not in st.session_state:
+    st.session_state.text_chat_enabled = False
+    st.session_state.messages = []
+
+
+st.sidebar.toggle(
+    ":material/keyboard: Enable Text Chat",
+    value=st.session_state.text_chat_enabled,
+    on_change=toggle_text_chat,
+)
+
+if st.session_state.text_chat_enabled:
+    show_messages()
+
+if message := handle_audio():
+    st.session_state.messages.append(message)
+    if st.session_state.text_chat_enabled:
+        with st.chat_message(name="Bot"):
+            st.markdown(message)
